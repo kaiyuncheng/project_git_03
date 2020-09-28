f8794fcb9bb6e72f5b0f918d5e6ebf31fea301a6
KaiYun Cheng <tanyacheng@gmail.com>
2020-09-28 19:54:53 +0800

    新增JS功能二

--- all.js
+++ all.js
@@ -1,3 +1,4 @@
+//JS功能ㄧ
 function print3x15() {
   for (x = 3; x < 16; x++) {
     for (y = 1; y < 16; y++) {
@@ -6,3 +7,11 @@ function print3x15() {
   }
 };
 console.log(print3x15());
+
+
+//JS功能二
+function BMI(height, weight) {
+  const bmi = (weight / Math.pow(height / 100, 2)).toFixed(1);
+  console.log(bmi);
+}
+
